c42946c0bebd010c128dbab5e1b984700bf47f43
ShaikhMohammadRizwan <your_email@example.com>
2024-12-10 23:04:06 +0530

    Files Added

---
+++ pythonLearning/firstProgramm.py
@@ -0,0 +1,14 @@
+print("Hello World")
+# Hello World
+
+print('20+30')
+# 20+30
+
+print(30 + 20)
+# 50
+
+print("Hello", "Rizwan")
+# Hello Rizwan
+
+print(19>9)
+# True
---
+++ pythonLearning/whatIsOperatorInPython.py
@@ -0,0 +1,12 @@
+***** Operators keyPoints *****
+
+1. Arithimetic Operator.
+2. Assignment Operator
+3. Comparision Operator
+4. Logical Operator
+5. Identity Operator
+6. Membership Operator
+7. Bitwise Operator
+
+******************************
+
